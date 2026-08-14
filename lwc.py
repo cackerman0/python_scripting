e15f4bb080e1ef3e4633cb5f485b1d76475ace3b
Cole Ackerman <coleack1@gmail.com>
2026-08-13 20:38:01 -0400

    Read from argument

--- lwc.py
+++ lwc.py
@@ -1,4 +1,10 @@
-file = open('file.txt', 'r')
+import argparse
+
+parser = argparse.ArgumentParser(description='Read contents of a file.')
+parser.add_argument('file_path', help='Path to the file to read.')
+args = parser.parse_args()
+
+file = open(args.file_path, 'r')
 f = file.readlines()
 
 # Count number of lines
