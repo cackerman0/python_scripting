df4c18648d66928b3ca2a7043ce34f13e230e6b2
Cole Ackerman <coleack1@gmail.com>
2026-08-13 20:24:03 -0400

    Complete and Tested

--- lwc.py
+++ lwc.py
@@ -1,4 +1,12 @@
 file = open('file.txt', 'r')
-f = file.read()
-print(f)
+f = file.readlines()
+
+# Count number of lines
+print("Lines:", len(f))
+
+# Count number of words
+print("Words:", sum(len(line.split()) for line in f))
+
+# Count number of characters
+print("Characters:", sum(len(line) for line in f))
 file.close()
